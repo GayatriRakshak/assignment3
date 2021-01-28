a43f41398e50483b0fdc2778b25d0c647d247b32
MICKEY935974 <69594578+MICKEY935974@users.noreply.github.com>
2021-01-28 20:44:25 +0530

    Add files via upload

---
+++ positive number.py
@@ -0,0 +1,13 @@
+list1=[23,-27,-87,44,-4,12]
+for x in list1:
+    if x>0:
+        print(x, end=' ')
+
+print('\n')
+list2=[ 13,-2,-8,40,-10,70]
+for y in list2:
+    if y>0:
+        print(y, end=' ')
+
+               
+
